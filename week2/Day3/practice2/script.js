d498ffd733fdda3d64589db93ce04ffccfab7d83
THC <thomas040120@gmail.com>
2026-06-24 12:37:34 +0800

    feat: 完成 Week2 Day3 練習一

--- week2/Day3/practice2/script.js
+++ week2/Day3/practice2/script.js
@@ -1,8 +1,18 @@
 for (let i = 1; i <= 30; i++) {
     let result;
-    if ((i % 15) === 0) result = "FizzBuzz";
-    else if ((i % 5) === 0) result = "Buzz";
-    else if ((i % 3) === 0) result = "Fizz";
+    if (i % 15 === 0) result = "FizzBuzz";
+    else if (i % 5 === 0) result = "Buzz";
+    else if (i % 3 === 0) result = "Fizz";
     else result = i;
     console.log(result);
 }
+
+/*
+進階解法 
+for (let i = 1; i <= 30; i++) {
+    let result = "";
+    if (i % 3 === 0) result += "Fizz"; 先判是不是 3 的倍數
+    if (i % 5 === 0) result += "Buzz"; 再判是不是 5 的倍數
+    console.log(result || i); // ""是 falsy，所以非特定倍數時會用 i。
+}
+*/
